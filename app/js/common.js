f2583b971ded7cde0d751f6ce57df68351c7cec2
Максим Ткачев <maximtkachev99@gmail.com>
2020-05-12 01:50:17 +0400

    init

---
+++ app/js/common.js
@@ -0,0 +1,26 @@
+$(function() {
+
+$('.animals__item-box').mouseenter(function() {
+	if ($(this).hasClass('active')) return;
+
+	$('.animals__item-box.active').removeClass('active');
+	$(this).addClass('active');
+	$('.animals__image').attr('src', $(this).data('image'));
+});
+
+$('.js-modal-open').click(function(e) {
+	e.preventDefault();
+	$('body').addClass('overflow-hidden');
+	$( $(this).attr('href') ).fadeIn();
+});
+
+$('.modal__close').click(function() {
+	$('body').removeClass('overflow-hidden');
+	$(this).parents('.modal-wrapper').fadeOut();
+});
+
+$('.header__nav-toggle').click(function() {
+	$('.header__nav-list').slideToggle(200);
+});
+
+});
